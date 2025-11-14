1188aa4e2aa85bc449952caa50a13d88e5a4f29d
Sam-IPID <samuel.psantos19@senacsp.edu.br>
2025-11-13 21:46:03 -0300

    Atualização do Portfolio

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ if (texto.style.color === "red") {
 }
 
 texto.addEventListener("click", mudar);
+
+const img = document.getElementById('foto');
+
+img.tabindex = 0;
+
+img.addEventListener('click',() => {
+    img.classList.toggle('is-expanded')
+})
+
+img.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === ' '){
+        img.classList.toggle('is-expanded');
+        e.preventDefault();
+    }
+})
+
+img.addEventListener('mouseenter',() => img.classList.add('is-expanded'))
+img.addEventListener('mouseleave',() => img.classList.remove('is-expanded'))
