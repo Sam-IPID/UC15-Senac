190dcff00bcd65d5f9d980ccb489b5b81f0e74e2
Sam-IPID <samuel.psantos19@senacsp.edu.br>
2025-12-01 21:13:38 -0300

    Atualização do Portfolio 2.0

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const texto = document.getElementById("texto");
 
 function mudar() {
-if (texto.style.color === "red") {
-    texto.style.color = "blue";
-} else {
-    texto.style.color = "red";
-}
+    if (texto.style.color === "red") {
+        texto.style.color = "blue";
+    } else {
+        texto.style.color = "red";
+    }
 }
 
 texto.addEventListener("click", mudar);
 
 const img = document.getElementById('foto');
 
-img.tabindex = 0;
+img.tabIndex = 0;
 
-img.addEventListener('click',() => {
+img.addEventListener('click', () => {
     img.classList.toggle('is-expanded')
-})
+});
 
 img.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' || e.key === ' '){
+    if (e.key === 'Enter' || e.key === ' ') {
         img.classList.toggle('is-expanded');
         e.preventDefault();
     }
-})
+});
 
-img.addEventListener('mouseenter',() => img.classList.add('is-expanded'))
-img.addEventListener('mouseleave',() => img.classList.remove('is-expanded'))
+img.addEventListener('mouseenter', () => img.classList.add('is-expanded'));
+img.addEventListener('mouseleave', () => img.classList.remove('is-expanded'));
